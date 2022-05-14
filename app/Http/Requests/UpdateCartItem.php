b7f5a64c943c63ef8df0950d50c2562c671dcd06
JessieCheng <hamy820326@gmail.com>
2022-05-14 23:25:20 +0800

    fix: validation fail then redirection

--- app/Http/Requests/UpdateCartItem.php
+++ app/Http/Requests/UpdateCartItem.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateCartItem extends FormRequest
+class UpdateCartItem extends APIRequest
 {
     /**
      * Determine if the user is authorized to make this request.
